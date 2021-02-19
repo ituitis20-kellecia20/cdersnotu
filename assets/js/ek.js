f8527019564d722d9f13a7e20f7fefeaec9f69c2
ituitis20-kellecia20 <kellecia20@itu.edu.tr>
2021-02-19 17:17:41 +0300

    giriş

--- assets/js/ek.js
+++ assets/js/ek.js
@@ -11,3 +11,16 @@ $(function() {
      position: "right"    
    });
  });
+
+
+ var firebaseConfig = {
+  apiKey: "AIzaSyA5CfC0YipdJDl2EuIlRk4hu8Ht8ffdLQc",
+  authDomain: "cdersnotu.firebaseapp.com",
+  projectId: "cdersnotu",
+  storageBucket: "cdersnotu.appspot.com",
+  messagingSenderId: "7745115915",
+  appId: "1:7745115915:web:de3dbd94656b4d2c88f40e",
+  measurementId: "G-W2K0HFRQ0R"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();

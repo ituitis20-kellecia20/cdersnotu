f9325a113a7f4dc9843707e05b18805fd4fbe60a
ituitis20-kellecia20 <kellecia20@itu.edu.tr>
2021-02-19 18:16:28 +0300

    deneme iptal7

--- assets/js/ek.js
+++ assets/js/ek.js
@@ -13,17 +13,6 @@ $(function() {
  });
 
 
- var firebaseConfig = {
-  apiKey: "AIzaSyA5CfC0YipdJDl2EuIlRk4hu8Ht8ffdLQc",
-  authDomain: "cdersnotu.firebaseapp.com",
-  projectId: "cdersnotu",
-  storageBucket: "cdersnotu.appspot.com",
-  messagingSenderId: "7745115915",
-  appId: "1:7745115915:web:de3dbd94656b4d2c88f40e",
-  measurementId: "G-W2K0HFRQ0R"
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 
 
@@ -33,34 +22,3 @@ firebase.analytics();
 
 
 
-
-
-var provider = new firebase.auth.GoogleAuthProvider();
-
-function googleSignin() {
-   firebase.auth()
-   
-   .signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-		
-      console.log(token)
-      console.log(user)
-   }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-		
-      console.log(error.code)
-      console.log(error.message)
-   });
-}
-
-function googleSignout() {
-   firebase.auth().signOut()
-	
-   .then(function() {
-      console.log('Signout Succesfull')
-   }, function(error) {
-      console.log('Signout Failed')  
-   });
-}

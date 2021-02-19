8f9a98721b1965cb51e3c3b3f2b88fd025b05e73
ituitis20-kellecia20 <kellecia20@itu.edu.tr>
2021-02-19 17:01:57 +0300

    küçük problemler çözüldü

--- assets/js/ek.js
+++ assets/js/ek.js
@@ -5,7 +5,7 @@ $(function() {
    $('#WAButton').floatingWhatsApp({
      phone: '+905393633677',
      headerTitle: 'Bana Ulaş!', 
-     popupMessage: 'Nasıl yardımcı olabilirim?', 
+     popupMessage: 'Arkadaş olmak mı istiyorsun? Mesaj at.', 
      showPopup: true,
      buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
      position: "right"    
